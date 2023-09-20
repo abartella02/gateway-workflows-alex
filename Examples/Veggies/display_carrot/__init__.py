a0c202ea1b8e9dbce8a8da22b618f80a73877abf
abartella <abartella@bluecatneworks.com>
2023-09-20 17:04:14 -0400

    added custom workflows

---
+++ Examples/Veggies/display_carrot/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2020 BlueCat Networks (USA) Inc. and its affiliates. All Rights Reserved.
+# -*- coding: utf-8 -*-
+
+type = 'ui'
+sub_pages = [
+    {
+        'name'        : 'display_fruit_page',
+        'title'       : u'display_fruit',
+        'endpoint'    : 'display_fruit/display_fruit_endpoint',
+        'description' : u'Displays a banana'
+    },
+]
