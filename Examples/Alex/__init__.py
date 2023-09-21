43d5faf8da83bc00367287a20d4b74c23d306dff
abartella <abartella@bluecatneworks.com>
2023-09-21 18:22:37 -0400

    restructure tree

---
+++ Examples/Alex/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2020 BlueCat Networks (USA) Inc. and its affiliates. All Rights Reserved.
+# -*- coding: utf-8 -*-
+
+type = 'ui'
+sub_pages = [
+    {
+        'name'        : 'Alex_page',
+        'title'       : u'Alex',
+        'endpoint'    : 'Alex/Alex_endpoint',
+        'description' : u'Alex - Software Developer Co-op'
+    },
+]
---
+++ Examples/Solid/js/ice_page.js
@@ -0,0 +1,4 @@
+// Copyright 2020 BlueCat Networks (USA) Inc. and its affiliates. All Rights Reserved.
+$(document).ready(function() {
+
+});
